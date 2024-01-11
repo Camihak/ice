a2a31007be81ba8324535c1aff9729e2f5b23419
Camihak <105364337+Camihak@users.noreply.github.com>
2024-01-11 11:18:10 +0300

    Add lines

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,7 @@
 const { getCommitlintConfig } = require('@applint/spec');
 
 module.exports = getCommitlintConfig('common');
+
+const { getCommitlintConfig } = require('@applint/spec');
+
+module.exports = getCommitlintConfig('common');
